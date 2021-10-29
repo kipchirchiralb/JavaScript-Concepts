52883d8edd6536b2d16c98427bb3fb312d9c2bc7
kipchirchiralbert <albertkipchirchir02@gmail.com>
2021-10-30 02:16:55 +0300

    day 72, came back after a break from coding to do my pgde1.1 cats, which led to a serious burnout, but now back stronger and more willing than ever

--- fetchAPI/main.js
+++ fetchAPI/main.js
@@ -9,3 +9,24 @@ fetch("demo.json")
 
 fetch("https://developer.mozilla.org/en-US/docs/Web/API/Blob")
     .then(respo => console.log(respo.text))
+
+
+
+let users = fetch("https://reqres.in/api/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: "User 1"
+        })
+    })
+    .then(res => {
+        if (res.ok) {
+            console.log("Succesful")
+            return res.json()
+        } else {
+            console.log("Not Succesful")
+        }
+    })
+    .then(data => console.log(data.name))
